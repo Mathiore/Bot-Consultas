6afad24f6a36cc5ea86ce46253edd6341563ffeb
mathiore <mraimundo@edu.univali.br>
2022-09-15 18:03:04 -0300

    verifica tela de bloqueio

--- bot-single.py
+++ bot-single.py
@@ -10,13 +10,3 @@ py.keyDown('alt')
 py.press('tab')
 py.keyUp('alt')
 
-
-
-while 1:
-    icon_pos = py.locateOnScreen('alertaconcluido.png')
-    if icon_pos:
-        py.click(x=1054, y=597)
-        print('Consulta Finalizada')
-        break
-        
-py.click(x=1326, y=278)
--- bot-telaconsulta.py
+++ bot-telaconsulta.py
@@ -23,32 +23,46 @@ time.sleep(17)
 
 #CONFIGURANDO DATA
 py.click(x=392, y=405)
-py.write('21')
+py.write('16')
 py.press('-')
 py.write('08')
 py.press('tab')
-py.write('25')
+py.write('20')
 py.press('-')
 py.write('08')
 
-#py.click(x=382, y=477)
+#Comando para a letra C
+py.click(x=398, y=715)
 
-py.click(x=415, y=588)
-n = 4
-i = 0
+i=0
+n = 19
+while i <= n:
+    py.press('down')
+    i = i+1
 
-while i < n:
+j = 0
+x = 19
+while j < x:
     py.press('space')
 
     py.press('down', presses=2)
 
-    i = i+1
+    j = j+1
 
 py.click(x=414, y=342)
 
+
 while 1:
     icon_pos = py.locateOnScreen('alertaconcluido.png')
-    if icon_pos:
+    blockicon_pos = py.locateOnScreen('teladebloqueio.png')
+    if blockicon_pos:
+        py.click(x=1556, y=251)
+        print('Consultas bloqueadas')
+        py.click(x=42, y=35)
+        py.click(x=40, y=111)
+        py.click(x=961, y=591)
+        break
+    elif icon_pos:
         py.click(x=1054, y=597)
         print('Consulta Finalizada')
         break
